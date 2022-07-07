4ebde8ca311dcdd2cde4126acd9f3343eb924f39
Lyubava0382 <lubovkrivencova@gmail.com>
2022-07-07 18:17:39 +0300

    Astar: new step

--- app/src/main/java/application/app/Tests.java
+++ app/src/main/java/application/app/Tests.java
@@ -12,25 +12,24 @@ public class Tests {
                 //FirstTestMinF(atest) + "\n" +
                 FirstTestAStar(atest) + "\n" +
                 FirstTestPath(atest) + "\n" +
-                FirstTestWeight(atest) +"\n";
+                FirstTestWeight(atest) +"\n" +
+                FirstTestOpen(atest) +"\n" +
+                FirstTestClosed(atest);
     }
 
     private static String FirstTestH() {
-        System.out.println();
         if (AStar.h(new Vertex('a', 0, 0), new Vertex('b', 0, 0)) == 0.0)
             return "First test: heuristic." + "\tAnswer is correst. Test: OK";
         else return "First test: heuristic." + "\tAnswer is incorrest. Test failed";
     }
 
     private static String SecondTestH() {
-        System.out.println();
         if (Math.abs(AStar.h(new Vertex('a', 100.234, 2), new Vertex('b', 189.2, 0)) - 88.98847765862723) < 0.0000000001)
             return "Second test: heuristic." + "\tAnswer is correst. Test: OK";
         else return "Second test: heuristic." + "\tAnswer is incorrest. Test failed";
     }
 
     private static String ThirdTestH() {
-        System.out.println();
         if (Math.abs(AStar.h(new Vertex('a', 100, 300), (new Vertex('b', 100 + Math.sqrt(600), 305))) - 25) < 0.0000000001)
             return "Third test: heuristic." + "\tAnswer is correst. Test: OK";
         else return "Third test: heuristic." + "\tAnswer is incorrest. Test failed";
@@ -58,7 +57,6 @@ public class Tests {
     }
 */
     private static String FirstTestAStar(AStar n) {
-        System.out.println();
         Graph graph = GraphController.readGraphForTests("2\na 0 0\nb 0 0\na b 0");
         ArrayList<Vertex> test = n.a_star_public(graph.findVertex('a'), graph.findVertex('b'), graph.getVertexes());
         ArrayList<Vertex> array = new ArrayList<>();
@@ -69,20 +67,43 @@ public class Tests {
     }
 
     private static String FirstTestPath(AStar n) {
-        System.out.println();
         if (n.getPath().equals("ab")) return "First test: Path." + "\tAnswer is correst. Test: OK";
         else return "First test: Path." + "\tAnswer is incorrest. Test failed";
     }
 
     private static String FirstTestWeight(AStar n) {
-        System.out.println();
         if (Math.abs(n.getWeight() - 0) < 0.0000000001) return "First test: Weight." + "\tAnswer is correst. Test: OK";
         else return "First test: Weight." + "\tAnswer is incorrest. Test failed";
     }
 
-//    private static String FirstTestOpen() {
-//        System.out.println();
-//        ArrayList<String> strings = new ArrayList<>();
-//        ArrayList<String> test_strings = new ArrayList<>();
-//    }
+    private static String FirstTestOpen(AStar n) {
+        ArrayList<String> strings = new ArrayList<>();
+        String a;
+        String test_strings = "";
+        for (ArrayList<Vertex> i : n.getOpen())
+        {   a = "";
+            for (Vertex k: i){
+                a += Character.toString(k.getName());
+            }
+            test_strings += a;
+        }
+        if (test_strings.equals("b"))
+        return "First test: Open." + "\tAnswer is correst. Test: OK";
+        else return "First test: Open." + "\tAnswer is incorrest. Test failed";
+    }
+    private static String FirstTestClosed(AStar n) {
+        ArrayList<String> strings = new ArrayList<>();
+        String a;
+        String test_strings = "";
+        for (ArrayList<Vertex> i : n.getClose())
+        {   a = "";
+            for (Vertex k: i){
+                a += Character.toString(k.getName());
+            }
+            test_strings += a;
+        }
+        if (test_strings.equals("a"))
+            return "First test: Open." + "\tAnswer is correst. Test: OK";
+        else return "First test: Open." + "\tAnswer is incorrest. Test failed";
+    }
 }
