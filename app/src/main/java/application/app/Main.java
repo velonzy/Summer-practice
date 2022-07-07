5f8d51589cf49f76537362300fb032839a41a8b4
Detkova Anna <71722131+detkovaannya@users.noreply.github.com>
2022-07-07 19:15:22 +0300

    Add files via upload

--- app/src/main/java/application/app/Main.java
+++ app/src/main/java/application/app/Main.java
@@ -27,4 +27,8 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 800, 550));
         primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
