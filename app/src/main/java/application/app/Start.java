be0c0ece768f01531b9e8aa4dea4bcc8ae562af8
zdobnovakseniya <kzdobnova2001@gmail.com>
2022-07-10 19:32:43 +0300

    fixing....

--- app/src/main/java/application/app/Start.java
+++
@@ -1,7 +0,0 @@
-package application.app;
-
-public class Start {
-    public static void main(String[] args) {
-        Main.main(args);
-    }
-}
--- app/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module application.app {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens application.app to javafx.fxml;
-    exports application.app;
-}
